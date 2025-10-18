5ee694ee12a37ba8f0d713a437c933ce1edfe032
Filipesantos-web <filipesbds13@gmail.com>
2025-10-18 17:12:51 -0300

    Add files via upload

---
+++ listas.py
@@ -0,0 +1,14 @@
+x=int(input("Quantos elementos tem as listas?"))
+lista1=[0]*x
+lista2=[0]*x
+lista3=[0]*x
+print("Lendo a primeira lista")
+for i in range (x):
+    lista1[i]=int(input(f"Digite o valor do {i+1} elemento"))
+print("Lendo a segunda lista")
+for i in range (x):
+    lista2[i]=int(input(f"Diginte o valor do {i+1} elemento"))
+print("Calculando a soma dos elementos")
+for i in range (x):
+    lista3[i]=lista1[i]+lista2[i]
+    print(lista3)
