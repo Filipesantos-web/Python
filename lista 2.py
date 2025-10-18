902b70a4ee3675a91656b0dc75718b4d80683918
Filipesantos-web <filipesbds13@gmail.com>
2025-10-18 16:19:08 -0300

    Add files via upload

---
+++ lista 2.py
@@ -0,0 +1,9 @@
+n=int(input("Digite quantas elementos possui a lista:"))
+lista=[0]*n 
+for i in range(n):
+    lista[i]=int(input(f"Digite um {i+1} valor:"))
+print(lista)
+print(sorted(lista))
+print(len(lista))
+print(sum(lista))
+print(lista)
